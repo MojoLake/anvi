53c852adf290297245d1fcc9df2c8642c06cbe6e
Elias Simojoki <simo.simojoki@gmail.com>
2026-08-30 00:32:10 +0200

    refactor: move registry and display to coldwrite state to make it easier to clean up the state

--- src/main.c
+++ src/main.c
@@ -18,8 +18,11 @@ struct coldwrite_state {
     struct wl_compositor *wl_compositor;
     struct coldwrite_output *outputs;
     struct wl_shm *wl_shm;
+    struct wl_display *display;
+    struct wl_registry *registry;
     bool initialization_failed;
 };
+
 
 static void destroy_output_proxy(struct wl_output *output) {
     if (output == NULL) {
@@ -46,6 +49,34 @@ static void destroy_outputs(struct coldwrite_state *state) {
     }
 
     state->outputs = NULL;
+}
+
+static void destroy_coldwrite_state(struct coldwrite_state *state) {
+    destroy_outputs(state);
+    if (state->wl_shm != NULL) {
+        wl_shm_destroy(state->wl_shm);
+        state->wl_shm = NULL;
+    }
+
+    if (state->wl_compositor != NULL) {
+        wl_compositor_destroy(state->wl_compositor);
+        state->wl_compositor = NULL;
+    }
+
+    if (state->session_lock_manager != NULL) {
+        ext_session_lock_manager_v1_destroy(state->session_lock_manager);
+        state->session_lock_manager = NULL;
+    }
+
+    if (state->registry != NULL) {
+        wl_registry_destroy(state->registry);
+        state->registry = NULL;
+    } 
+
+    if (state->display != NULL) {
+        wl_display_disconnect(state->display);    
+        state->display = NULL;
+    }
 }
 
 static void registry_global(
@@ -190,8 +221,8 @@ int main(void) {
 
     struct coldwrite_state state = {0};
 
-   	struct wl_display *display = wl_display_connect(NULL);
-  	if (display == NULL) {
+   	state.display = wl_display_connect(NULL);
+  	if (state.display == NULL) {
 		fprintf(stderr, "Unable to connect to the Wayland compositor\n");
 
 		return EXIT_FAILURE;
@@ -199,63 +230,53 @@ int main(void) {
     
 
    	printf("Connected to the Wayland compositor!\n");
-	struct wl_registry *registry = wl_display_get_registry(display);
+	state.registry = wl_display_get_registry(state.display);
 
-	if (registry == NULL) {
+	if (state.registry == NULL) {
 		fprintf(stderr, "Unable to obtain the Wayland registry\n");
-		wl_display_disconnect(display);
+        destroy_coldwrite_state(&state);
 		return EXIT_FAILURE;
    	}
 
-    if (wl_registry_add_listener(registry, &registry_listener, &state) < 0) {
+    if (wl_registry_add_listener(state.registry, &registry_listener, &state) < 0) {
         fprintf(stderr, "Unable to install the registry listener\n");
-        wl_registry_destroy(registry);
-        wl_display_disconnect(display);    
+        destroy_coldwrite_state(&state);
         return EXIT_FAILURE;
     }
 
-    if (wl_display_roundtrip(display) < 0) {
+    if (wl_display_roundtrip(state.display) < 0) {
         fprintf(stderr, "Wayland communication failed.\n");
-        wl_registry_destroy(registry);
-        wl_display_disconnect(display);
+        destroy_coldwrite_state(&state);
         return EXIT_FAILURE;
     }
 
     if (state.outputs == NULL) {
         fprintf(stderr, "No outputs were found...\n");
-        wl_registry_destroy(registry);
-        wl_display_disconnect(display);
+        destroy_coldwrite_state(&state);
         return EXIT_FAILURE;
     }
 
     if (state.wl_shm == NULL) {
         fprintf(stderr, "Wayland shared memory (wl_shm) not available...\n");
-        wl_registry_destroy(registry);
-        wl_display_disconnect(display);
+        destroy_coldwrite_state(&state);
         return EXIT_FAILURE;
     }
 
     if (state.initialization_failed) {
         fprintf(stderr, "Something went wrong with initialization, exiting...\n");
-        wl_registry_destroy(registry);
-        destroy_outputs(&state);
-        wl_display_disconnect(display);
+        destroy_coldwrite_state(&state);
         return EXIT_FAILURE;
     }
 
     if (state.session_lock_manager == NULL) {
         fprintf(stderr, "Session locking is not supported\n");
-        wl_registry_destroy(registry);
-        destroy_outputs(&state);
-        wl_display_disconnect(display);
+        destroy_coldwrite_state(&state);
         return EXIT_FAILURE;
     }
 
     if (state.wl_compositor == NULL) {
         fprintf(stderr, "The wl_compositor was not found\n");
-        wl_registry_destroy(registry);
-        destroy_outputs(&state);
-        wl_display_disconnect(display);
+        destroy_coldwrite_state(&state);
         return EXIT_FAILURE;
     }
 
@@ -265,12 +286,7 @@ int main(void) {
         ext_session_lock_manager_v1_interface.version
     );
 
-    ext_session_lock_manager_v1_destroy(state.session_lock_manager);
-    wl_compositor_destroy(state.wl_compositor);
-    destroy_outputs(&state);
-    wl_registry_destroy(registry);
-    wl_shm_destroy(state.wl_shm);
-    wl_display_disconnect(display);
+    destroy_coldwrite_state(&state);
 
 	return EXIT_SUCCESS;
 }
